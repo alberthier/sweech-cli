1179d8ba71f6fa49cb6353e3eff59439f6ec2a0d
Éric ALBER <eric.alber@gmail.com>
2017-04-16 22:58:36 +0200

    Add setup script

---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup
+
+def read(fname):
+    import os
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name = "sweech-cli",
+    version = "1.0.0",
+    description = "A command-line utility to interact with your Android device through the Sweech app",
+    long_description = read('README.md'),
+    url = 'https://github.com/alberthier/sweech-cli',
+    license = 'MIT',
+    author = 'Eric ALBER',
+    author_email = 'eric.alber@gmail.com',
+    classifiers = [
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Information Technology',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+    ],
+    py_modules = ['sweech'],
+    entry_points = {
+        'console_scripts': [
+            'sweech = sweech:_main',
+        ]
+    }
+)
